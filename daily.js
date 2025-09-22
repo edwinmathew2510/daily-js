849b25e3af8a6e7af803cc1247322a76a82f9773
Edwin Mathew <edwintm2510@gmail.com>
2025-09-22 20:25:26 +0530

    day 1

---
+++ daily.js
@@ -0,0 +1,12 @@
+//Write a program that takes two numbers and prints their sum.
+
+let a = 10;
+let b = 20;
+
+// function add(a, b) {
+//   return a + b;
+// }
+// console.log(add(a, b));
+
+const add = (a, b) => a + b;
+console.log(add(a, b));

d27aa8189a39972e511432a7fb80d31e2a57a422
Edwin Mathew <edwintm2510@gmail.com>
2025-10-15 12:36:51 +0530

    JavaScript program to count how many vowels

--- daily.js
+++ daily.js
@@ -1,24 +1,15 @@
-// day 10
-// 'JavaScript program to remove duplicate values from an array without using Set().'
+// day 11
+// JavaScript program to count how many vowels (a, e, i, o, u) are in a given string.
 
-let arr = [1, 2, 2, 3, 4, 4, 5];
+let str = "JavaScript is awesome";
 
-let output = [];
+let lowerCaseStr = str.toLowerCase().replace(/\s/g, "").split("");
 
-for (let i = 0; i < arr.length; i++) {
-  if (!output.includes(arr[i])) {
-    output.push(arr[i]);
+let count = 0;
+
+for (let i = 0; i < lowerCaseStr.length; i++) {
+  if ("aeiou".includes(lowerCaseStr[i])) {
+    count = count + 1;
   }
 }
-console.log(output);
-
-// Find the Sum of All Numbers in an Array
-
-let arr1 = [10, 20, 30, 40, 50];
-count = 0;
-
-for (let i = 0; i <= arr1.length - 1; i++) {
-  count += arr1[i];
-}
-
-console.log(count);
+console.log(`There ${count === 1 ? "is" : "are"} ${count} vowels.`);

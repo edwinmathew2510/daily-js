1f0c788ff608f0e3a51dd514911c30d085a43368
Edwin Mathew <edwintm2510@gmail.com>
2025-10-23 17:37:52 +0530

    // day 18 :string pattern

--- daily.js
+++ daily.js
@@ -1,11 +1,28 @@
-// 🧩 Day 17 — Problem: Check if Two Strings are Anagrams
+// day 18 :string pattern
 
-function Anagrams(str1, str2) {
-  if (str1.length !== str2.length) return false;
-  str1 = str1.replace(/\s/g, "").toLowerCase();
-  str2 = str2.replace(/\s/g, "").toLowerCase();
-
-  return str1.split("").sort().join("") === str2.split("").sort().join("");
+let row;
+let n = 11;
+for (let i = 0; i < n; i++) {
+  row = "";
+  for (let j = 0; j < n; j++) {
+    if (
+      i == 0 ||
+      i == n - 1 ||
+      j == 0 ||
+      j == n - 1 ||
+      i == Math.floor(n / 2) ||
+      j == Math.floor(n / 2) ||
+      i + j == n - 1 ||
+      i == j ||
+      i + j == Math.floor(n / 2) ||
+      i - j == Math.floor(n / 2) ||
+      j - i == Math.floor(n / 2) ||
+      j + i == n - 1 + Math.floor(n / 2)
+    ) {
+      row += "* ";
+    } else {
+      row += "  ";
+    }
+  }
+  console.log(row);
 }
-let result = Anagrams("listen", "silent");
-console.log(result);

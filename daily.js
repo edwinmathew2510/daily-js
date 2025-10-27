874e7236d91ee7b1cd4442b6628f090c83324b6b
Edwin Mathew <edwintm2510@gmail.com>
2025-10-27 14:04:22 +0530

    day 19 counter fuction

--- daily.js
+++ daily.js
@@ -1,28 +1,11 @@
-// day 18 :string pattern
-
-let row;
-let n = 11;
-for (let i = 0; i < n; i++) {
-  row = "";
-  for (let j = 0; j < n; j++) {
-    if (
-      i == 0 ||
-      i == n - 1 ||
-      j == 0 ||
-      j == n - 1 ||
-      i == Math.floor(n / 2) ||
-      j == Math.floor(n / 2) ||
-      i + j == n - 1 ||
-      i == j ||
-      i + j == Math.floor(n / 2) ||
-      i - j == Math.floor(n / 2) ||
-      j - i == Math.floor(n / 2) ||
-      j + i == n - 1 + Math.floor(n / 2)
-    ) {
-      row += "* ";
-    } else {
-      row += "  ";
-    }
+function createCounter(){
+  let value =0
+  function increment(){
+   return ++value
   }
-  console.log(row);
+  return {increment}
 }
+let counter1=createCounter()
+let counter2=createCounter()
+console.log(counter1.increment())
+console.log(counter1.increment())

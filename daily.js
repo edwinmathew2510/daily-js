9baabdc72b74e1f00e1cd08969f78927784de06e
Edwin Mathew <edwintm2510@gmail.com>
2025-11-12 20:17:57 +0530

    day 28,2621. Sleep

--- daily.js
+++ daily.js
@@ -1,12 +1,8 @@
-// day 27,2723. Add Two Promises
+// day 28,2621. Sleep
 
-addTwoPromises = async function (promise1, promise2) {
-  let prm1 = await promise1;
-  let prm2 = await promise2;
+async function sleep(millis) {
+  return new Promise((resolve) => setTimeout(resolve, millis));
+}
 
-  return prm1 + prm2;
-};
-
-let p1 = new Promise((resolve) => setTimeout(() => resolve(10), 50));
-let p2 = new Promise((resolve) => setTimeout(() => resolve(-12), 30));
-addTwoPromises(p1, p2).then(console.log);
+let t = Date.now();
+sleep(2000).then(() => console.log(Date.now() - t)); // 100

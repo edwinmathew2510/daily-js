c6976125fb18d3b9ee9dc05a1e629b8ef9cf6c22
Edwin Mathew <edwintm2510@gmail.com>
2025-11-16 20:38:44 +0530

    day 29 /2725. Interval Cancellation

--- daily.js
+++ daily.js
@@ -1,7 +1,16 @@
-//day 29,2715. Timeout Cancellation
-var cancellable = function (fn, args, t) {
-  let timer = setTimeout(() => {
+// day 29 /2725. Interval Cancellation
+let cancellable = function (fn, args, t) {
+  fn(...args);
+
+  const id = setInterval(() => {
     fn(...args);
   }, t);
-  return (cancelFn = () => clearTimeout(timer));
+
+  return function cancelFn() {
+    clearInterval(id);
+  };
 };
+
+const cancelTimeMs = 190;
+const cancelFn = cancellable((x) => x * 2, [4], 35);
+setTimeout(cancelFn, cancelTimeMs);

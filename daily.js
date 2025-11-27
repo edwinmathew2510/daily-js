93521743dd2bc4c273e2aaca3b0993ef1635cb16
Edwin Mathew <edwintm2510@gmail.com>
2025-11-27 12:20:08 +0530

    day 31,2727. Is Object Empty

--- daily.js
+++ daily.js
@@ -1,20 +1,9 @@
-// day 31,2721. Execute Asynchronous Functions in Parallel
-function promiseAll(functions) {
-  return new Promise(async (res, rej) => {
-    let len = functions.length;
-    let count = 0;
-    let ans = new Array(len);
-    functions.forEach(async (fun, index) => {
-      try {
-        let val = await fun();
-        ans[index] = val;
-        count++;
-        if (count === len) res(ans);
-      } catch (err) {
-        rej(err);
-      }
-    });
-  });
-}
-const promise = promiseAll([() => new Promise((res) => res(42))]);
-promise.then(console.log); // [42]
+// day 31,2727. Is Object Empty
+const isEmpty = function(obj) {
+if(Array.isArray(obj))return obj.length===0
+let arr=Object.keys(obj)
+return arr.length===0
+};
+
+console.log(isEmpty([null, false, 0]))
+console.log(isEmpty({}))

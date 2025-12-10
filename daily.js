3a7ebde05d5178e03f43fd327cec7580139b2814
Edwin Mathew <edwintm2510@gmail.com>
2025-12-10 15:59:15 +0530

    new linux

--- daily.js
+++ daily.js
@@ -1,7 +1,3 @@
-// day 35 , 2724. Sort By
-
-function sortby(fn,arr){
-    return arr.sort((a,b)=>fn(a)-fn(b))
-}
-let s=sortby((x) => x,[5,4,1,2,3])
-console.log(s)
+//hey new linux mint 
+//checking everthing
+console.log("hey linux")

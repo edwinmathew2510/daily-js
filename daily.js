97581a47178f455ff87369a2ee7bceaecbe08df0
Edwin Mathew <edwintm2510@gmail.com>
2025-12-12 13:25:00 +0530

    day 36,2722. Join Two Arrays by ID

--- daily.js
+++ daily.js
@@ -1,3 +1,30 @@
-//hey new linux mint 
-//checking everthing
-console.log("hey linux")
+//day 36,2722. Join Two Arrays by ID
+
+function sortedArr(arr1,arr2){
+
+    let map= new Map()
+
+    for(const obj of arr1){
+        map.set(obj.id,{...obj})
+    }
+    for(const obj of arr2){
+        if(map.has(obj.id)){
+            map.set(obj.id,{...map.get(obj.id),...obj})
+        }
+        else{
+            map.set(obj.id,{...obj})
+        }
+    }
+    return Array.from(map.values()).sort((a,b)=>a.id-b.id)
+}
+
+
+
+let res=sortedArr([
+  { id: 1, x: 2 },
+  { id: 2, x: 9 }
+],[
+  { id: 1, x: 2 },
+  { id: 3, x: 9 }
+])
+console.log(res)
